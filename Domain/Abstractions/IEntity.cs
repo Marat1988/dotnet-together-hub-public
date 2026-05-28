51ec53439a746e64e510b7432674cb04a6417765
Marat <pirat03071988@mail.ru>
2026-05-28 23:31:28 +0200

    2.1 - 2.2)  Подготовка инфраструктуры. Первая сущность.

---
+++ Domain/Abstractions/IEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Abstractions
+{
+
+    public interface IEntity<T> : IEntity
+    {
+        public T Id { get; set; }
+    }
+
+
+    public interface IEntity
+    {
+    }
+}
---
+++ Domain/ValueObjects/Location.cs
@@ -0,0 +1,22 @@
+﻿namespace Domain.ValueObjects
+{
+    public record Location
+    {
+        public string City { get; set; } = default!;
+        public string Street { get; set; } = default!;
+
+
+        private Location(string city, string street)
+        {
+            City = city;
+            Street = street;
+        }
+
+        public static Location Of(string city, string street)
+        {
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(city);
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(street);
+            return new Location(city, street);
+        }
+    }
+}
