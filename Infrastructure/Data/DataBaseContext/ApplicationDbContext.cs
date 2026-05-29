5a053b48c3f040b02732f6007ef83761c2cefbe6
Marat <pirat03071988@mail.ru>
2026-05-29 16:26:58 +0200

    2.3 - 2.4. GlobalUsing. DbContext

---
+++ Infrastructure/Data/DataBaseContext/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Application.Data.DataBaseContext;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data.DataBaseContext
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public DbSet<Topic> Topics => Set<Topic>();
+
+
+        public ApplicationDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+    }
+}
